fd18f5c0e77343e122d54618c0ff27544ef21b79
Diogo Bandeira <diogohenriquefb@gmail.com>
2023-11-02 14:22:33 -0300

    adicionado o arquivo index.html e pastas

---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+/* 
+O que precisamos fazer? - quando clicar no botão do personagem na lista temos que marcar o botão como selecionado e mostrar o personagem correspondente
+    
+    OBJETIVO 1 - quando clicar no botão do personagem na lista marcar o botão como selecionado
+        passo 1 - pegar os botões no JS para poder verificar quando o usuário clicar em cima de um deles
+        passo 2 - adicionar a classe "selecionado" no botão que o usuário clicou
+        passo 3 - verificar se já existe um botão selecionado, se sim, devemos remover a selecão dele
+        
+    OBJETIVO 2 - quando clicar no botão do personagem mostrar as informações do personagem
+        passo 1 - pegar os personagens no JS para poder mostrar ou esconder ele
+        passo 2 - adicionar a classe "selecionado" no personagem que o usuário selecionou
+        passo 3 - verificar se já exista um personagem selecionado, se sim, devemos remover sua seleção
+*/
+
+// OBJETIVO 1 - quando clicar no botão do personagem na lista marcar o botão como selecionado
+
+//passo 1 - pegar os botões no JS para poder verificar quando o usuário clicar em cima de um deles
+const botoes = document.querySelectorAll('.botao');
+
+// OBJETIVO 2 - quando clicar no botão do personagem mostrar as informações do personagem
+
+// passo 1 - pegar os personagens no JS para poder mostrar ou esconder ele
+const personagens = document.querySelectorAll('.personagem');
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        // passo 3 - verificar se já existe um botão selecionado, se sim, devemos remover a selecão dele7
+
+        const botaoSelecionado = document.querySelector(".botao.selecionado");      
+        botaoSelecionado.classList.remove("selecionado"); 
+
+
+        // passo 2 - adicionar a classe "selecionado" no botão que o usuário clicou
+       botao.classList.add("selecionado");
+
+       const personagemSelecionado = document.querySelector(".personagem.selecionado");
+       personagemSelecionado.classList.remove('selecionado')
+
+       personagens[indice].classList.add('selecionado');
+       
+    });
+});
+
+
